b3380db71707f029e75dd05c30172d3b8c7d79f2
Tenzin Jorden <tenden96@gmail.com>
2021-12-01 14:20:07 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    name = cats.push("Ralph");
+
+}
+function destructivelyPrependCat(name){
+    name = cats.unshift("Bob");
+
+}
+function destructivelyRemoveLastCat(name){
+    name = cats.pop();
+
+}
+function destructivelyRemoveFirstCat(name){
+    name = cats.shift();
+
+}
+function appendCat(name) {
+    const catsArray = [...cats, "Broom"];
+    return catsArray
+}
+
+function prependCat(name){
+    const catsArray = ["Arnold", ...cats];
+    return catsArray;
+}
+
+function removeLastCat(name){
+    const catsArray = cats.slice(0, cats.length - 1);
+    return catsArray;
+}
+
+function removeFirstCat(name){
+    const catsArray = [...cats.slice(1)];
+    return catsArray;
+
+}
